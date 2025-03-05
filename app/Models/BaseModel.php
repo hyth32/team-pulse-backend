b74a527be6810909b9f45f02171fc43703b99d78
Ivan Goryaev <5474n.sage@gmail.com>
2025-03-05 15:57:59 +0300

    добавил определение tableName в базовой модели

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -3,13 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Str;
+use ReflectionClass;
+use Illuminate\Support\Str;
 
 class BaseModel extends Model
 {
     protected $keyType = 'string';
     public $incrementing = false;
     public $timestamps = true;
+
+    public function getTable()
+    {
+        if (!empty($this->table)) {
+            return $this->table;
+        }
+
+        $reflection = new ReflectionClass($this);
+        $className = $reflection->getShortName();
+
+        return Str::snake(Str::plural($className));
+    }
 
     protected static function boot()
     {
